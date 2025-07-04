17f3da5fe6284b8c81a0aaf28279a1af9a1905dc
flashyflash369 <draftsman369@gmail.com>
2025-07-04 22:00:07 +0200

    final-commit

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,19 +1,74 @@
 using UnityEngine;
-
+using KBCore.Refs;
+using System;
+using System.Collections;
+using Unity.Cinemachine;
 namespace Platformer
 {
-    public class CameraManager : MonoBehaviour
+    public class CameraManager : ValidatedMonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+
+        [SerializeField, Anywhere] InputReader input;
+        [SerializeField, Anywhere] CinemachineCamera freeLookVCam;
+
+        [Header("Settings")]
+
+        [SerializeField, Range(0.5f, 3f)] float speedMultiplier;
+
+        bool isRMBPressed;
+        bool isDeviceMouse;
+        bool cameraMovementLock;
+
+
+        void OnEnable()
         {
-        
+            input.Look += OnLook;
+            input.EnableMouseControlCamara += OnEnableMouseControlCamera;
+            //input.DisableMouseControlCamara += OnDisableMouseControlCamera;
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnEnableMouseControlCamera()
         {
-        
+            isRMBPressed = true;
+
+            //Lock the cursor o the center of the screen and hide it
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            StartCoroutine(DisableMouseForFrame());
         }
+
+        private void OnDisableMouseControlCamera()
+        {
+            isRMBPressed = false;
+
+            //Lock the cursor o the center of the screen and hide it
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            //freeLookVCam.
+        }
+
+        IEnumerator DisableMouseForFrame()
+        {
+            isDeviceMouse = true;
+            yield return new WaitForEndOfFrame(); // Wait for the next frame
+            cameraMovementLock = false;
+
+        }
+
+        private void OnLook(Vector2 arg0, bool arg1)
+        {
+            throw new NotImplementedException();
+        }
+
+        void OnDisable()
+        {
+            input.Look -= OnLook;
+            input.EnableMouseControlCamara -= OnEnableMouseControlCamera;
+            //input.DisableMouseControlCamara -= OnDisableMouseControlCamera;
+        }
+
+        
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,90 @@
+using KBCore.Refs;
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Platformer
 {
-    public class PlayerController : MonoBehaviour
+    public class PlayerController : ValidatedMonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+
+        [Header("References")]
+        [SerializeField, Self] CharacterController controller;
+        [SerializeField, Anywhere] Animator animator;
+        [SerializeField, Anywhere] CinemachineCamera freeLookVcam;
+        [SerializeField, Anywhere] InputReader input;
+
+        [Header("Settings")]
+        [SerializeField] float moveSpeed = 6f;
+        [SerializeField] float rotationSpeed = 5f;
+        [SerializeField] float smoothTime = 0.2f; //For smooting animation
+
+        const float Zerof = 0.0f;
+
+        Transform mainCam;
+
+        float currentSpeed;
+        float velocity;
+
+        // Animator parameters
+        static readonly int Speed = Animator.StringToHash("Speed");
+
+        void Awake()
         {
-        
+            mainCam = Camera.main.transform;
+            freeLookVcam.Follow = transform;
+            freeLookVcam.LookAt = transform;
+            freeLookVcam.OnTargetObjectWarped(transform, transform.position - Vector3.forward);
         }
 
-        // Update is called once per frame
         void Update()
         {
-        
+            HandleMovement();
+            HandleAnimation();
+        }
+
+        void HandleMovement()
+        {
+            var movementDirection = new Vector3(input.Direction.x, 0, input.Direction.y).normalized;
+            // Rotate the player towards the camera direction
+            var adjustedDirection = Quaternion.AngleAxis(mainCam.eulerAngles.y, Vector3.up) * movementDirection;
+
+            if (adjustedDirection.magnitude > Zerof)
+            {
+
+                HandleRotation(adjustedDirection);
+                HandleCharacterController(adjustedDirection);
+                SmoothSpeed(adjustedDirection.magnitude);
+            }
+            else
+            {
+                SmoothSpeed(Zerof);
+            }
+        }
+
+        void HandleAnimation()
+        {
+            animator.SetFloat(Speed, currentSpeed);
+        }
+
+        void SmoothSpeed(float value)
+        {
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, value, ref velocity, smoothTime);
+        }
+
+        void HandleRotation(Vector3 adjustedDirection)
+        {
+            var targetRotation = Quaternion.LookRotation(adjustedDirection);
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            transform.LookAt(transform.position + adjustedDirection);
+        }
+
+        void HandleCharacterController(Vector3 adjustedDirection)
+        {
+            //Move the player
+            var adjustedMovement = adjustedDirection * moveSpeed * Time.deltaTime;
+            controller.Move(adjustedMovement);
         }
     }
 }
